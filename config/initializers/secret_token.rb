dd228b0e3f6538662b27882e12525cf2c9846b51
Virtuatech <contact@virtuatech.fr>
2012-07-24 09:53:06 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TutoApp::Application.config.secret_token = '70a91040fde4412891380a42aeadee8ea709de7c99a2e9528b0062fd1cdda696397a71b9af9fa3047ef4bae15fd106a0c0fa3bb4bc34fbceff9f0f4a314bcb91'
